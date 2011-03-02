a6053452dfbd3120a26784299d6000a9e8e89729
Tommy Andersen <toan@itu.dk>
2011-03-02 16:03:15 +0000

    Added config OSGi project.
    git-svn-id: svn://home.tommya.net/tommy/itu/thesis/trunk/projects/frigga@770 0e72c7a4-4771-45a2-ae65-a574b2f066c5

---
+++ core/config/src/dk/itu/frigga/config/InvalidXMLConfigException.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dk.itu.frigga.config;
+
+/**
+ *
+ * @author Tommy
+ */
+public class InvalidXMLConfigException extends ConfigSyntaxException {
+
+}
