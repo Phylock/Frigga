f065b0c820d88790a39875e4a8403b7cafb3456a
Mikkel Wendt-Larsen <miwe@itu.dk>
2011-03-16 09:35:36 +0000

    Added some more unversioned filetypes to svn ignore list Fixed DogDriver, its now up and running again ...
    git-svn-id: svn://home.tommya.net/tommy/itu/thesis/trunk/projects/frigga@802 0e72c7a4-4771-45a2-ae65-a574b2f066c5

--- plugins/EliteDogDriver/src/dk/itu/frigga/device/drivers/dog/DogDriver.java
+++ plugins/EliteDogDriver/src/dk/itu/frigga/device/drivers/dog/DogDriver.java
@@ -23,99 +23,100 @@ import org.osgi.service.log.LogService;
  * @author phylock
  */
 public class DogDriver implements Driver {
-  //External services, initialized by DependencyManager
+    //External services, initialized by DependencyManager
 
-  private volatile DeviceManager devicemanager;
-  private volatile LogService log;
-  //Private member variables
-  private Connection connection = null;
-  
-  /*** TODO: read parameter, for now assume that the Dog gateway is running on localhost */
-  public static String DEFAULT_DOG_ADDRESS = "http://localhost:65300/RPC2";
+    private volatile DeviceManager devicemanager;
+    private volatile LogService log;
+    //Private member variables
+    private Connection connection = null;
+    /*** TODO: read parameter, for now assume that the Dog gateway is running on localhost */
+    public static String DEFAULT_DOG_ADDRESS = "http://localhost:65300/RPC2";
 
-  public DogDriver() {
-
-  }
-
-  public Connection getConnection() {
-    return connection;
-  }
-
-  /** Implements Driver***/
-  @Override
-  public FunctionResult callFunction(Device[] devices, Executable function, Parameter... parameters)
-          throws UnknownDeviceException, InvalidFunctionException, InvalidParameterException {
-    return function.execute(devices, parameters);
-  }
-
-  public void update() {
-    try {
-      DogMessage command = DogProtocol.generateDescribeDevice(null);
-      connection.send(command);
-    } catch (ParserConfigurationException ex) {
-      log.log(LogService.LOG_WARNING, null, ex);
-    }
-  }
-
-  public void update(String[] devicecategories) {
-    try {
-      DogMessage command = DogProtocol.generateDescribeDeviceCategory(devicecategories);
-      connection.send(command);
-    } catch (ParserConfigurationException ex) {
-      log.log(LogService.LOG_WARNING, null, ex);
-    }
-  }
-
-  public void update(DeviceId[] devices) {
-    try {
-      int length = devices.length;
-      String[] d = new String[length];
-      for (int i = 0; i < length; i++) {
-        d[i] = devices[i].toString();
-      }
-      DogMessage command = DogProtocol.generateDescribeDevices(d);
-      connection.send(command);
-    } catch (ParserConfigurationException ex) {
-      log.log(LogService.LOG_WARNING, null, ex);
-    }
-  }
-
-  private void validate() {
-    if (connection == null) {
-      connection = new Connection(this);
+    public DogDriver() {
     }
 
-    updateSubclassFields(Connection.class, "log", connection, log);
-    updateSubclassFields(DogParser.class, "log", connection.getParser(), log);
-
-    connection.getParser().setDeviceManager(devicemanager);
-
-
-    connection.connect(DogDriver.DEFAULT_DOG_ADDRESS);
-
-
-
-  }
-
-  private void invalidate() {
-    connection.disconnect();
-  }
-
-  /**
-   * Update referance in subclass, even if its private
-   * @param clazz the class to update
-   * @param field the field name
-   * @param obj the class instance to update
-   * @param value the new value
-   */
-  private void updateSubclassFields(Class clazz, String field, Object obj, Object value) {
-    try {
-      Field f = clazz.getDeclaredField("log");
-      f.setAccessible(true);
-      f.set(connection, log);
-      f.setAccessible(false);
-    } catch (Exception ex) {
-      log.log(LogService.LOG_WARNING, String.format("Could not propergate iPOJO field %s to subclass %s", field, obj.getClass().getName()));
+    public Connection getConnection() {
+        return connection;
     }
-  }
+
+    /** Implements Driver***/
+    @Override
+    public FunctionResult callFunction(Device[] devices, Executable function, Parameter... parameters)
+            throws UnknownDeviceException, InvalidFunctionException, InvalidParameterException {
+        return function.execute(devices, parameters);
+    }
+
+    public void update() {
+        try {
+            DogMessage command = DogProtocol.generateDescribeDevice(null);
+            connection.send(command);
+        } catch (ParserConfigurationException ex) {
+            log.log(LogService.LOG_WARNING, null, ex);
+        }
+    }
+
+    public void update(String[] devicecategories) {
+        try {
+            DogMessage command = DogProtocol.generateDescribeDeviceCategory(devicecategories);
+            connection.send(command);
+        } catch (ParserConfigurationException ex) {
+            log.log(LogService.LOG_WARNING, null, ex);
+        }
+    }
+
+    public void update(DeviceId[] devices) {
+        try {
+            int length = devices.length;
+            String[] d = new String[length];
+            for (int i = 0; i < length; i++) {
+                d[i] = devices[i].toString();
+            }
+            DogMessage command = DogProtocol.generateDescribeDevices(d);
+            connection.send(command);
+        } catch (ParserConfigurationException ex) {
+            log.log(LogService.LOG_WARNING, null, ex);
+        }
+    }
+
+    private void validate() {
+        if (connection == null) {
+            connection = new Connection(this);
+        }
+        log.log(LogService.LOG_INFO, "DogDriver Validate");
+        updateSubclassFields(Connection.class, "log", connection, log);
+        updateSubclassFields(DogParser.class, "log", connection.getParser(), log);
+
+        connection.getParser().setDeviceManager(devicemanager);
+
+
+        connection.connect(DogDriver.DEFAULT_DOG_ADDRESS);
+
+
+
+    }
+
+    private void invalidate() {
+        connection.disconnect();
+    }
+
+    /**
+     * Update referance in subclass, even if its private
+     * @param clazz the class to update
+     * @param field the field name
+     * @param obj the class instance to update
+     * @param value the new value
+     */
+    private void updateSubclassFields(Class clazz, String field, Object obj, Object value) {
+        try {
+            Field f = clazz.getDeclaredField(field);
+            boolean access = f.isAccessible();
+            
+            f.setAccessible(true);
+            f.set(obj, value);
+            f.setAccessible(access);
+
+        } catch (Exception ex) {
+            log.log(LogService.LOG_WARNING, String.format("Could not propergate iPOJO field %s to subclass %s", field, obj.getClass().getName()));
+        }
+    }
 }
