0e5e87c7dc7564fa0e75c42b2da1cf3d17eacea3
Mikkel Wendt-Larsen <miwe@itu.dk>
2011-05-18 11:16:11 +0000

    moved clientprotocol project to libraries
    git-svn-id: svn://home.tommya.net/tommy/itu/thesis/trunk/projects/frigga@906 0e72c7a4-4771-45a2-ae65-a574b2f066c5

--- core/clientprotocol/src/dk/itu/frigga/protocol/InvalidMagicNumberException.java
+++
@@ -1,9 +0,0 @@
-package dk.itu.frigga.protocol;
-
-/**
- * Created by IntelliJ IDEA. User: Tommy Date: 18-04-11 Time: 23:55 To change this template use File | Settings | File
- * Templates.
- */
-public class InvalidMagicNumberException extends ProtocolException
-{
-}
--- core/clientprotocol/src/dk/itu/frigga/protocol/StopDiscoveringException.java
+++
@@ -1,9 +0,0 @@
-package dk.itu.frigga.protocol;
-
-/**
- * Created by IntelliJ IDEA. User: Tommy Date: 13-04-11 Time: 21:28 To change this template use File | Settings | File
- * Templates.
- */
-public class StopDiscoveringException extends ProtocolException
-{
-}
